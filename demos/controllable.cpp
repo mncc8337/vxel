68076b56cce114759a7b5dc1add30224624aabb2
mncc8337 <mncc8327@gmail.com>
2023-12-09 21:25:41 +0700

    chores?

--- demos/controllable.cpp
+++ demos/controllable.cpp
@@ -1,9 +1,12 @@
+// rendering 1000000 voxels
+
 #include <vxel/vxel.hpp>
-#include <iostream>
 
 vxel engine(800, 800, "test");
 
 const float speed = 10.0f;
+
+// function to run before rendering each frame
 void func() {
     if(engine.keyhold[GLFW_KEY_ESCAPE])
         engine.stop();
@@ -40,9 +43,12 @@ const uint IDK = engine.addTexture("./textures/dcm.png");
 int main(void) {
     engine.loadShader();
 
-    engine.camera.position = glm::vec3(0.0f, 0.0f, 100.0f);
+    engine.camera.position = glm::vec3(-100.0f, 50.0f, 300.0f);
+    engine.camera.far = 1e6;
+    engine.camera.fovy = 45;
+    engine.camera.yaw += 25.0f;
 
-    int volume = 50;
+    int volume = 100; // ^3
     for(int i = 0; i < volume; i++)
         for(int j = 0; j < volume; j++)
             for(int k = 0; k < volume; k++)
